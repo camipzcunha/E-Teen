7807fb1756d5897f12f32974cbbda072abf0b2ed
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-16 20:22:50 -0300

    rotas atualizadas +outlet

---
+++ src/Paginas/HomePage/index.jsx
@@ -0,0 +1,9 @@
+import Hero from "../../Components/Hero";
+
+export default function HomePage(){
+    return(
+        <>
+            <Hero/>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+//imports para rotas
+import ReactDOM from 'react-dom/client'
+import React from 'react'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+
+//import paginas
+import Sobre from './Paginas/Sobre/index.jsx'
+import HomePage from './Paginas/HomePage/index.jsx'
+
+//import Componente
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>,
+    children:[
+      {index:true, element:<HomePage/>},
+      {path:'/Sobre', element:<Sobre/>}
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
