0acc559b03a089a8bea5b06a676ae25bb4ed3981
Pedro Camacho <pedroalmeidacamacho@gmail.com>
2024-10-31 12:03:08 -0300

    Roletou

--- src/Paginas/Roleta/index.jsx
+++ src/Paginas/Roleta/index.jsx
@@ -1,29 +1,82 @@
 import { useState } from 'react';
 import { Wheel } from 'react-custom-roulette';
+import HeaderLogin from '../../Components/HeaderLogin';
+import Footer from '../../Components/Footer';
+
 
 
 const data = [
-    { option: 'nyck Dvries', style: { backgroundColor: 'red', textColor: 'black' } },
-    { option: 'ayrton senna', style: { backgroundColor: 'white', textColo: 'black' } },
-    { option: '2' },
-]
+  { option: 'Nyck de Vries' },
+  { option: '1000' },
+  { option: 'Jake Hughes', style: { textColor: '#f9dd50' } },
+  { option: 'Sam Bird' },
+  { option: 'Mais um Giro' },
+  { option: 'Edoardo Mortara' },
+  { option: '3 Aleatorias', style: { textColor: '#70bbe0' } },
+  { option: 'Antonio Feliz' },
+];
+
+const backgroundColors = ['#ff8f43', '#70bbe0', '#0b3351', '#f9dd50'];
+const textColors = ['#0b3351'];
+const outerBorderColor = '#eeeeee';
+const outerBorderWidth = 10;
+const innerBorderColor = '#30261a';
+const innerBorderWidth = 0;
+const innerRadius = 0;
+const radiusLineColor = '#eeeeee';
+const radiusLineWidth = 8;
+const fontFamily = 'Nunito';
+const fontWeight = 'bold';
+const fontSize = 20;
+const fontStyle = 'normal';
+const textDistance = 60;
+const spinDuration = 1.0;
 export default function Roleta() {
 
-    const [mustSpin, setMustSpin] = useState(false)
-    function handleSpin() {
-        const myTimeout = setTimeout(setMustSpin(true), 5000);
-        setMustSpin(false)
-        clearTimeout(myTimeout);
-    }
-    return (<div>
-        <Wheel
-            mustStartSpinning={mustSpin}
-            prizeNumber={3}
-            data={data}
-            backgroundColors={['#3e3e3e', '#df3428']}
-            textColors={['#ffffff']}
+    const [mustSpin, setMustSpin] = useState(false);
+    const [prizeNumber, setPrizeNumber] = useState(0);
+  
+    const handleSpinClick = () => {
+      if (!mustSpin) {
+        const newPrizeNumber = Math.floor(Math.random() * data.length);
+        setPrizeNumber(newPrizeNumber);
+        setMustSpin(true);
+      }
+    };
+    return (<div >
+        <HeaderLogin/>
+        <section className='flex justify-center align-middle m-12'> 
+        <Wheel 
+          mustStartSpinning={mustSpin}
+          prizeNumber={prizeNumber}
+          data={data}
+          backgroundColors={backgroundColors}
+          textColors={textColors}
+          fontFamily={fontFamily}
+          fontSize={fontSize}
+          fontWeight={fontWeight}
+          fontStyle={fontStyle}
+          outerBorderColor={outerBorderColor}
+          outerBorderWidth={outerBorderWidth}
+          innerRadius={innerRadius}
+          innerBorderColor={innerBorderColor}
+          innerBorderWidth={innerBorderWidth}
+          radiusLineColor={radiusLineColor}
+          radiusLineWidth={radiusLineWidth}
+          spinDuration={spinDuration}
+          startingOptionIndex={Math.random()*7}
+          
+          textDistance={textDistance}
+          onStopSpinning={() => {
+            setMustSpin(false);
+            console.log(prizeNumber)
+          }}
         />
-        <button onClick={handleSpin()}></button>
+         <button className='text-white' onClick={handleSpinClick}>
+          SPIN
+        </button>
+        </section>
+        <Footer/>
     </div>)
 
 
